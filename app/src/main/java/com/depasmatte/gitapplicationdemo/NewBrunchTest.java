bdb545191555edefe9225e15a914185a3b87ebd6
Matteo Depasquali <Matteo Depasquali>
2018-05-23 15:46:20 +0200

    prova aggiunta brunch chiamato dev

---
+++ app/src/main/java/com/depasmatte/gitapplicationdemo/NewBrunchTest.java
@@ -0,0 +1,13 @@
+package com.depasmatte.gitapplicationdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewBrunchTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_brunch_test);
+    }
+}
