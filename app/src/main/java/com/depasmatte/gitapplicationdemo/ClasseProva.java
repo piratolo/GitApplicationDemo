e1b7a762ca943f13363df8f514c987cd8b4605a9
Matteo Depasquali <Matteo Depasquali>
2018-05-25 10:15:40 +0200

    modifica numero 1 del metodo ciao nella  Classe Prova

--- app/src/main/java/com/depasmatte/gitapplicationdemo/ClasseProva.java
+++ app/src/main/java/com/depasmatte/gitapplicationdemo/ClasseProva.java
@@ -7,6 +7,7 @@ public class ClasseProva {
 
     public void ciao(Context context){
         Toast.makeText(context,  "Sono il metodo ciao", Toast.LENGTH_SHORT).show();
+        String a = "ciao";
 
     }
 
